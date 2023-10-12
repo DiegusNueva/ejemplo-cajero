f2b2a164a78972d48ea111695dd4c14a15df8fda
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-10-12 16:31:01 +0200

    Aplicamos el .js

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const balance = 1000;
+    let currentBalance = balance;
+
+    const messageElement = document.getElementById("message");
+    const balanceElement = document.getElementById("balance");
+    const amountElement = document.getElementById("amount");
+    const withdrawButton = document.getElementById("withdraw");
+    const depositButton = document.getElementById("deposit");
+
+    withdrawButton.addEventListener("click", function () {
+        const amount = parseFloat(amountElement.value);
+        if (isNaN(amount) || amount <= 0 || amount > currentBalance) {
+            messageElement.textContent = "Monto no válido";
+        } else {
+            currentBalance -= amount;
+            updateBalance();
+            messageElement.textContent = `Retirado: $${amount}`;
+        }
+    });
+
+    depositButton.addEventListener("click", function () {
+        const amount = parseFloat(amountElement.value);
+        if (isNaN(amount) || amount <= 0) {
+            messageElement.textContent = "Monto no válido";
+        } else {
+            currentBalance += amount;
+            updateBalance();
+            messageElement.textContent = `Depositado: $${amount}`;
+        }
+    });
+
+    function updateBalance() {
+        balanceElement.textContent = `Saldo: $${currentBalance}`;
+    }
+});
