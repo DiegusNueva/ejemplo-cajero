02b3d55d990f6c0fe43608e8d463ee58a78b4dc5
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-10-12 16:50:36 +0200

    Cambios de sintaxis al .js

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const balance = 1000;
-    let currentBalance = balance;
+document.addEventListener("DOMContentLoaded", () => {
+    const balance = 1000
+    let currentBalance = balance
 
-    const messageElement = document.getElementById("message");
-    const balanceElement = document.getElementById("balance");
-    const amountElement = document.getElementById("amount");
-    const withdrawButton = document.getElementById("withdraw");
-    const depositButton = document.getElementById("deposit");
+    const messageElement = document.getElementById("message")
+    const balanceElement = document.getElementById("balance")
+    const amountElement = document.getElementById("amount")
+    const withdrawButton = document.getElementById("withdraw")
+    const depositButton = document.getElementById("deposit")
 
-    withdrawButton.addEventListener("click", function () {
-        const amount = parseFloat(amountElement.value);
+    withdrawButton.addEventListener("click", () => {
+        const amount = parseFloat(amountElement.value)
         if (isNaN(amount) || amount <= 0 || amount > currentBalance) {
-            messageElement.textContent = "Monto no válido";
+            messageElement.textContent = "Monto no válido"
         } else {
-            currentBalance -= amount;
-            updateBalance();
-            messageElement.textContent = `Retirado: ${amount}€`;
+            currentBalance -= amount
+            updateBalance()
+            messageElement.textContent = `Retirado: ${amount}€`
         }
-    });
+    })
 
-    depositButton.addEventListener("click", function () {
-        const amount = parseFloat(amountElement.value);
+    depositButton.addEventListener("click", () => {
+        const amount = parseFloat(amountElement.value)
         if (isNaN(amount) || amount <= 0) {
-            messageElement.textContent = "Monto no válido";
+            messageElement.textContent = "Monto no válido"
         } else {
-            currentBalance += amount;
-            updateBalance();
-            messageElement.textContent = `Depositado: ${amount}€`;
+            currentBalance += amount
+            updateBalance()
+            messageElement.textContent = `Depositado: ${amount}€`
         }
-    });
+    })
 
-    function updateBalance() {
-        balanceElement.textContent = `Saldo: ${currentBalance}€`;
+    const updateBalance = () => {
+        balanceElement.textContent = `Saldo: ${currentBalance}€`
     }
-});
+})
